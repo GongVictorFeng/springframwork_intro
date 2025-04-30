c0d746f7da9c0128169b362682e703fa165b3c1a
victor <gfeng005@uottawa.ca>
2025-04-30 10:07:01 -0400

    initialized the application

---
+++ src/main/java/learn_spring/framework_intro/FrameworkIntroApplication.java
@@ -0,0 +1,13 @@
+package learn_spring.framework_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn_spring/framework_intro/FrameworkIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package learn_spring.framework_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
